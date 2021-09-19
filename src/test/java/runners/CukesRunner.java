28ecaf4f3ded9ec2614c16c94b55ce4c5e870468
Kiril <chiril.gherciu@gmail.com>
2021-09-18 20:01:43 -0500

    Initial commit

---
+++ src/test/java/runners/CukesRunner.java
@@ -0,0 +1,16 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)//This line will force this to RUN using cucumber
+@CucumberOptions (
+        plugin = "html:target/cucumber-report.html",
+        features = "src/test/resources/features",
+        glue = "step_definitions",
+        dryRun = false
+
+)
+public class CukesRunner {
+}
